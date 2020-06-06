693f859c8a974069d5c36706d82cfdb72ff954ac
Rurik Pinheiro <rurikspin@gmail.com>
2020-06-06 16:07:43 -0300

    feat: create chat application

---
+++ ch3/servers.js
@@ -0,0 +1,72 @@
+var WebSocket = require('ws');
+var WebSocketServer = require('ws').Server, wss = new WebSocketServer({port: 8181});
+var uuid = require('node-uuid');
+var clients = []
+var clientIndex = 1
+
+function wsSend(type, client_uuid, nickname, message) {
+    for(var i=0; i<clients.length; i++) {
+        var clientSocket = clients[i].ws;
+        if (clientSocket.readyState === WebSocket.OPEN) {
+            console.log(`client ${clients[i].id}: ${message}`)
+
+            clientSocket.send(JSON.stringify({
+                type,
+                id: client_uuid,
+                nickname, 
+                message}))
+        }
+    }
+}
+
+wss.on('connection', function(ws) {
+    var client_uuid = uuid.v4();
+    var nickname = `AnonymousUser ${clientIndex}`;
+    clientIndex+=1;
+    clients.push({"id": client_uuid, "ws": ws, "nickname": nickname });
+    console.log(`client ${client_uuid} connected`);
+
+    var connected_message = `${nickname} has connected`;
+
+    wsSend('notification', client_uuid, nickname, connected_message);
+
+    ws.on('message', function(message) {
+        if(message.indexOf('/nick') === 0) {
+            var nickname_array = message.split(' ')
+            if (nickname_array.length >=2) {
+                var old_nickname = nickname;
+                nickname = nickname_array[1]
+                var nickname_message = "Client " + old_nickname + " changed to " + nickname;
+                wsSend('nick_update', client_uuid, nickname, nickname_message)
+            }
+        } else {
+            wsSend('undefined', client_uuid, nickname, message);
+        }
+    });
+
+    var closeSocket = function(customMessage) {
+        for(var i=0; i<clients.length; i++) {
+            if(clients[i].id === client_uuid) {
+                var disconnected_message;
+                if (customMessage){
+                    disconnected_message = customMessage;
+                } else {
+                    disconnected_message = `${nickname} has disconnected`;
+                }
+                wsSend("notification", client_uuid, nickname, disconnected_message);
+            }
+        }
+    }
+
+    ws.on('close', function() {
+        closeSocket()
+    })
+
+    process.on('SIGINT', function() {
+        console.log("Closing things");
+        closeSocket("Server has disconnected");
+        process.exit();
+    })
+
+    
+});
